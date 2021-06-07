31e62a856983726e46a080a52a55e9526fc18147
vittoboa <boarinivittorio@gmail.com>
2021-06-07 22:35:35 +0200

    Add window resizing

--- main.c
+++ main.c
@@ -4,6 +4,14 @@
 // OpenGL global variables
 static GLsizei window_width = 1280, window_height = 720;
 
+// OpenGL window resize routine
+void resize(int new_width, int new_height)
+{
+    glViewport(0, 0, new_width, new_height);
+    window_width  = new_width;
+    window_height = new_height;
+}
+
 int main(int argc, char **argv)
 {
     // pass potential input arguments to glutInit
@@ -14,6 +22,7 @@ int main(int argc, char **argv)
     glutInitWindowSize(window_width, window_height);
     glutInitWindowPosition(0, 0);
     glutCreateWindow("Solar System");
+    glutReshapeFunc(resize);
 
     // add support for GLEW
     glewInit();

277bc02deb76a54fbe5611bf54d3daa752124c15
vittoboa <boarinivittorio@gmail.com>
2021-06-08 20:56:51 +0200

    Add projection

--- main.c
+++ main.c
@@ -4,6 +4,26 @@
 // OpenGL global variables
 static GLsizei window_width = 1280, window_height = 720;
 
+void init(void)
+{
+    // select clearing color
+    glClearColor(0.1, 0.1, 0.1, 0.0);
+
+    // enable depth test
+    glEnable(GL_DEPTH_TEST);
+
+    // initialize viewing values
+    glLoadIdentity();
+
+    // set projection
+    glMatrixMode(GL_PROJECTION);
+    const float ratio = (float) window_width / (float) window_height, scale = 1.2;
+    glFrustum(-(ratio * scale), ratio * scale, - scale, scale, 5.0, 60.0);
+
+    // initialize model view transforms
+    glMatrixMode(GL_MODELVIEW);
+}
+
 // OpenGL window resize routine
 void resize(int new_width, int new_height)
 {
@@ -26,4 +46,9 @@ int main(int argc, char **argv)
 
     // add support for GLEW
     glewInit();
+
+    init();
+    glutMainLoop();
+
+    return 0;
 }

260279005bd44f3d268f5972e7ba1ab41502c07a
vittoboa <boarinivittorio@gmail.com>
2021-08-16 17:12:49 +0200

    Turn the array of planets from global to local

--- main.c
+++ main.c
@@ -15,18 +15,8 @@ static GLsizei window_width = 1280, window_height = 720;
 // globals
 static const GLfloat planets_z = -30.0;  // planets position on z axis
 static const GLfloat light_pos[] = {0.0, 0.0, planets_z, 1.0};  // light coordinates
-static const int planets_num = 8;  // number of planets in the solar system
-static const int days_speed = 1;  // how many days are added at each iteration
-static Planet planets[] = {  // array to hold all planets
-    { .name = "Mercury", .x = 1.0,  .n_stacks = 10, .n_slices = 10, .current_day = 1, .days_in_year = 88,    .name_len = 7, .radius = 0.02439, .amb = {0.50, 0.40, 0.30, 1.0}, .diff = {1.0, 0.9, 0.8, 1.0}, .spec = {0.25, 0.20, 0.15, 1.0}, .shine = {20.0}},
-    { .name = "Venus",   .x = 2.0,  .n_stacks = 10, .n_slices = 10, .current_day = 1, .days_in_year = 225,   .name_len = 5, .radius = 0.06052, .amb = {0.50, 0.35, 0.20, 1.0}, .diff = {1.0, 0.7, 0.4, 1.0}, .spec = {0.25, 0.15, 0.10, 1.0}, .shine = {20.0}},
-    { .name = "Earth",   .x = 3.0,  .n_stacks = 10, .n_slices = 10, .current_day = 1, .days_in_year = 365,   .name_len = 5, .radius = 0.06371, .amb = {0.20, 0.20, 0.50, 1.0}, .diff = {0.4, 0.4, 1.0, 1.0}, .spec = {0.10, 0.10, 0.25, 1.0}, .shine = {20.0}},
-    { .name = "Mars",    .x = 4.0,  .n_stacks = 10, .n_slices = 10, .current_day = 1, .days_in_year = 687,   .name_len = 4, .radius = 0.03390, .amb = {0.50, 0.20, 0.10, 1.0}, .diff = {1.0, 0.4, 0.2, 1.0}, .spec = {0.25, 0.10, 0.05, 1.0}, .shine = {20.0}},
-    { .name = "Jupiter", .x = 6.0,  .n_stacks = 50, .n_slices = 50, .current_day = 1, .days_in_year = 4333,  .name_len = 7, .radius = 0.69911, .amb = {0.50, 0.35, 0.25, 1.0}, .diff = {1.0, 0.7, 0.5, 1.0}, .spec = {0.25, 0.20, 0.10, 1.0}, .shine = {20.0}},
-    { .name = "Saturn",  .x = 8.0,  .n_stacks = 50, .n_slices = 50, .current_day = 1, .days_in_year = 10759, .name_len = 6, .radius = 0.58232, .amb = {0.50, 0.45, 0.30, 1.0}, .diff = {1.0, 0.9, 0.6, 1.0}, .spec = {0.15, 0.10, 0.00, 0.2}, .shine = {20.0}},
-    { .name = "Uranus",  .x = 10.0, .n_stacks = 25, .n_slices = 25, .current_day = 1, .days_in_year = 30687, .name_len = 6, .radius = 0.25362, .amb = {0.20, 0.40, 0.50, 1.0}, .diff = {0.4, 0.8, 1.0, 1.0}, .spec = {0.10, 0.20, 0.25, 1.0}, .shine = {20.0}},
-    { .name = "Neptune", .x = 11.0, .n_stacks = 25, .n_slices = 25, .current_day = 1, .days_in_year = 60190, .name_len = 7, .radius = 0.24622, .amb = {0.10, 0.10, 0.50, 1.0}, .diff = {0.3, 0.3, 1.0, 1.0}, .spec = {0.05, 0.05, 0.25, 1.0}, .shine = {20.0}},
-};
+static const size_t planets_num = 8;  // number of planets in the solar system
+static const GLint days_speed = 1;  // how many days are added at each iteration
 
 // display routine
 void display(void)
@@ -37,11 +27,22 @@ void display(void)
     // clear color and depth buffer
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // Push initial state on the stack
+    // push initial state on the stack
     glPushMatrix();
 
     // turn on light
     glEnable(GL_LIGHT0);
+
+    static Planet planets[] = {  // array to hold all planets
+        { .name = "Mercury", .x = 1.0,  .n_stacks = 10, .n_slices = 10, .current_day = 1, .days_in_year = 88,    .name_len = 7, .radius = 0.02439, .amb = {0.50, 0.40, 0.30, 1.0}, .diff = {1.0, 0.9, 0.8, 1.0}, .spec = {0.25, 0.20, 0.15, 1.0}, .shine = {20.0}},
+        { .name = "Venus",   .x = 2.0,  .n_stacks = 10, .n_slices = 10, .current_day = 1, .days_in_year = 225,   .name_len = 5, .radius = 0.06052, .amb = {0.50, 0.35, 0.20, 1.0}, .diff = {1.0, 0.7, 0.4, 1.0}, .spec = {0.25, 0.15, 0.10, 1.0}, .shine = {20.0}},
+        { .name = "Earth",   .x = 3.0,  .n_stacks = 10, .n_slices = 10, .current_day = 1, .days_in_year = 365,   .name_len = 5, .radius = 0.06371, .amb = {0.20, 0.20, 0.50, 1.0}, .diff = {0.4, 0.4, 1.0, 1.0}, .spec = {0.10, 0.10, 0.25, 1.0}, .shine = {20.0}},
+        { .name = "Mars",    .x = 4.0,  .n_stacks = 10, .n_slices = 10, .current_day = 1, .days_in_year = 687,   .name_len = 4, .radius = 0.03390, .amb = {0.50, 0.20, 0.10, 1.0}, .diff = {1.0, 0.4, 0.2, 1.0}, .spec = {0.25, 0.10, 0.05, 1.0}, .shine = {20.0}},
+        { .name = "Jupiter", .x = 6.0,  .n_stacks = 50, .n_slices = 50, .current_day = 1, .days_in_year = 4333,  .name_len = 7, .radius = 0.69911, .amb = {0.50, 0.35, 0.25, 1.0}, .diff = {1.0, 0.7, 0.5, 1.0}, .spec = {0.25, 0.20, 0.10, 1.0}, .shine = {20.0}},
+        { .name = "Saturn",  .x = 8.0,  .n_stacks = 50, .n_slices = 50, .current_day = 1, .days_in_year = 10759, .name_len = 6, .radius = 0.58232, .amb = {0.50, 0.45, 0.30, 1.0}, .diff = {1.0, 0.9, 0.6, 1.0}, .spec = {0.15, 0.10, 0.00, 0.2}, .shine = {20.0}},
+        { .name = "Uranus",  .x = 10.0, .n_stacks = 25, .n_slices = 25, .current_day = 1, .days_in_year = 30687, .name_len = 6, .radius = 0.25362, .amb = {0.20, 0.40, 0.50, 1.0}, .diff = {0.4, 0.8, 1.0, 1.0}, .spec = {0.10, 0.20, 0.25, 1.0}, .shine = {20.0}},
+        { .name = "Neptune", .x = 11.0, .n_stacks = 25, .n_slices = 25, .current_day = 1, .days_in_year = 60190, .name_len = 7, .radius = 0.24622, .amb = {0.10, 0.10, 0.50, 1.0}, .diff = {0.3, 0.3, 1.0, 1.0}, .spec = {0.05, 0.05, 0.25, 1.0}, .shine = {20.0}},
+    };
 
     for (size_t i = 0; i < planets_num; ++i) {
         glPopMatrix();
